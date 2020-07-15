7f38ae856eca45320ee513878570c78279bdea7e
adityarahman-indocyber <aditya.rahman@indocyber.co.id>
2020-07-15 09:40:03 +0700

    test kreditplus

---
+++ AdityaRahman/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdityaRahman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 3;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+           
+            Console.WriteLine("Soal no 1 : ");
+            Console.WriteLine("Hasilnya A : {0} ", a);
+            Console.WriteLine("Hasilnya B : {0} ", b);
+
+            Console.WriteLine("\nSoal ke 2 : ");
+            int[] x = { 20, 10, 50, 46, 26, 87, 25, 5, 97, 24 };
+            int tampung;
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = i; j < x.Length; j++)
+                {
+                    if (x[i] > x[j])
+                    {
+                        tampung = x[i];
+                        x[i] = x[j];
+                        x[j] = tampung;
+                    }
+                }
+            }
+            Console.WriteLine("Perubahan nilai dari terkecil ke terbesar ");
+            Console.Write("Nilai : ");
+            for (int k = 0; k < x.Length; k++)
+            {
+                Console.Write("{0} ", x[k]);
+            }
+
+        }
+    }
+}
---
+++ AdityaRahman/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AdityaRahman")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdityaRahman")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91256a56-e661-4cb9-91c8-52e7f7351709")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
